7e536c7a3da564c8b3b2f36f853db56199d2c661
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-06 21:19:27 -0500

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/api', require('./api'));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.use(function (err, req, res, next) {
